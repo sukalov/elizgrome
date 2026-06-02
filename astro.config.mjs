f1d7f7f6630a966c44f213441a57714c21cc35ce
sukalov <matvey10@gmail.com>
2026-06-02 04:00:21 -0600

    deploy fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,16 @@ import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
-const owner = process.env.GITHUB_REPOSITORY_OWNER ?? 'username';
+const owner = process.env.GITHUB_REPOSITORY_OWNER ?? 'sukalov';
+const repo =
+  process.env.GITHUB_REPOSITORY?.split('/')[1] ??
+  process.env.GITHUB_REPOSITORY_NAME ??
+  'elizgrome';
 
 export default defineConfig({
   output: 'static',
   site: `https://${owner}.github.io`,
-  base: '/',
+  base: `/${repo}/`,
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
